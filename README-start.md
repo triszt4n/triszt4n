6b8677b1c780a4aaa70d42ea1ad29069520fb234
triszt4n <trisztanpiller@gmail.com>
2022-03-13 15:21:15 +0100

    Publish script

--- README-start.md
+++ README-start.md
@@ -5,5 +5,3 @@ My name's Trisztán Piller, I'm from Hungary. I'm currently working on my Comput
 I love exploring the latest web and app development technologies (mainly backend), would like to seek further knowledge in cloud computing. ☁️
 
 Get to know me on [my website](https://triszt4n.xyz/) ⚡
-
-## Current working tech stack
--- generate-readme.ts
+++ generate-readme.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs'
+import path from 'path'
 
 interface Tech {
   svgTitle: string
@@ -49,25 +50,25 @@ const toolsTech: Array<Tech> = [
 
 let content: string = ''
 try {
-  const dataStart = fs.readFileSync('./README-start.md', 'utf8')
-  content.concat(dataStart)
+  const dataStart = fs.readFileSync(path.join(__dirname, 'README-start.md'), 'utf8')
+  content = content.concat(dataStart)
 } catch (err) {
   console.error(err)
 }
 
-content.concat('\n\n## Tech stack I am currently working with\n\n')
-content.concat(...currentTech.map(generateDiv))
+content = content.concat('\n\n## Tech stack I am currently working with\n\n')
+content = content.concat(...currentTech.map(generateDiv))
 
-content.concat('\n\n## Tech stack I am experienced in\n\n')
-content.concat(...expTech.map(generateDiv))
+content = content.concat('\n\n## Tech stack I am experienced in\n\n')
+content = content.concat(...expTech.map(generateDiv))
 
-content.concat('\n\n## Favourite tools\n\n')
-content.concat(...toolsTech.map(generateDiv))
+content = content.concat('\n\n## Favourite tools\n\n')
+content = content.concat(...toolsTech.map(generateDiv))
 
 try {
-  const dataEnd = fs.readFileSync('./README-end.md', 'utf8')
-  content.concat('\n\n', dataEnd)
-  fs.writeFileSync('./README.md', content)
+  const dataEnd = fs.readFileSync(path.join(__dirname, 'README-end.md'), 'utf8')
+  content = content.concat('\n\n', dataEnd)
+  fs.writeFileSync(path.join(__dirname, 'README.md'), content, { encoding: 'utf8', flag: 'w' })
 } catch (err) {
   console.error(err)
 }
